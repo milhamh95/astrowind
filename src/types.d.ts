6e39d0966ef3bf03e6c5f93dbff813decd3efc8d
milhamh-al <113006665+milhamh-al@users.noreply.github.com>
2024-10-19 12:26:19 +0700

    feat: change post to portfolio post

--- src/types.d.ts
+++ src/types.d.ts
@@ -44,6 +44,49 @@ export interface Post {
   readingTime?: number;
 }
 
+export interface PortfolioPost {
+  /** A unique ID number that identifies a post. */
+  id: string;
+
+  /** A post’s unique slug – part of the post’s URL based on its name, i.e. a post called “My Sample Page” has a slug “my-sample-page”. */
+  slug: string;
+
+  /**  */
+  permalink: string;
+
+  /**  */
+  publishDate: Date;
+  /**  */
+  updateDate?: Date;
+
+  /**  */
+  title: string;
+  /** Optional summary of post content. */
+  excerpt?: string;
+  /**  */
+  image?: ImageMetadata | string;
+
+  /**  */
+  category?: Taxonomy;
+  /**  */
+  tags?: Taxonomy[];
+  /**  */
+  author?: string;
+
+  /**  */
+  metadata?: MetaData;
+
+  /**  */
+  draft?: boolean;
+
+  /**  */
+  Content?: AstroComponentFactory;
+  content?: string;
+
+  /**  */
+  readingTime?: number;
+}
+
 export interface Taxonomy {
   slug: string;
   title: string;
